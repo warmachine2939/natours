aacb61e29979a830efdef9d17bee939141163036
Shubhankar Dubey <shubhankardubey135@gmail.com>
2023-07-13 00:53:31 +0530

    Prepared App for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const scriptSrcUrls = [
   'https://unpkg.com/',
@@ -110,6 +111,7 @@ app.use(
   })
 );
 
+app.use(compression());
 //test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
